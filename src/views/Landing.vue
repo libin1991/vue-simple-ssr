a9ce6c1b95f046d0e8cd9e08c18ade398758f947
Kemal Yalcinkaya <kemalcany@gmail.com>
2017-12-16 09:47:34 +0000

    chore: views are removed, router simplified, blank landing page added

---
+++ src/views/Landing.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="landing">
+    <h1>Landing is here</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'landing',
+
+  components: {
+  },
+
+  props: {
+  },
+
+  computed: {
+  },
+
+  beforeMount () {
+  },
+
+  beforeDestroy () {
+  },
+
+  watch: {
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
