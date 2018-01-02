35c4b6d3785758532b2a915284cf7b26603ca4c9
Kemal Yalcinkaya <kemalcany@gmail.com>
2018-01-02 14:07:06 +0000

    feat: wip, infinity grid 50 images added with their thumbnails

--- src/components/InfinityGrid.vue
+++ src/components/InfinityGrid.vue
@@ -2,30 +2,35 @@
   <div class="infinity-grid">
     <p class="scroll">{{scroll}}</p>
     <div class="item"
-      v-for="item in gridItems" 
+      v-for="item in 50" 
       v-bind:key="item"
     >
-      <p>{{item}}</p>
+      <ssr-image class="item"
+        :src="'dist/static/images/workshop/' + item + '.jpg'"
+        :placeholder="'dist/static/images/workshop/thumbnails/' + item + '.jpg'"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import SSRImage from '../modules/ssr-progressive-img/components/SSRImage.vue'
 export default {
   name: 'infinityGrid',
+  components: {
+    'ssr-image': SSRImage
+  },
 
+  /*
   data: function () {
     return {
-      gridItems: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
     }
   },
+  */
 
   computed: {
     viewport () { return this.$store.getters['layout/viewport'] },
     scroll () { return this.$store.getters['layout/scroll'].position }
-  },
-
-  components: {
   },
 
   props: [
@@ -36,14 +41,6 @@ export default {
 
   methods: {
     loadMore: function () {
-      // this.busy = true;
-
-      setTimeout(() => {
-        for (var i = 50, j = 60; i < j; i++) {
-          this.gridItems.push({ name: 'abbas' })
-        }
-        // this.busy = false;
-      }, 1000)
     }
   }
 }
@@ -56,7 +53,6 @@ export default {
   grid-template-columns repeat(auto-fill, minmax(240px,1fr))
 .item
   width 100%
-  height 100px
   background #ededed
 .scroll
   position fixed
